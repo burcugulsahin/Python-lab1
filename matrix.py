6f7b041120595a00fb3056683fa2cd899e1d90af
burcugulsahin <burcugulsahin@gmail.com>
2017-04-12 16:01:44 +0200

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -38,22 +38,6 @@ class Matrix(object):
     def reset(self):
         """ Reset the matrix data """
         self.rows = [[] for x in range(self.m)]
-                     
-    def transpose(self):
-        """ Transpose the matrix. Changes the current matrix """
-        
-        self.m, self.n = self.n, self.m
-        self.rows = [list(item) for item in zip(*self.rows)]
-
-    def getTranspose(self):
-        """ Return a transpose of the matrix without
-        modifying the matrix itself """
-        
-        m, n = self.n, self.m
-        mat = Matrix(m, n)
-        mat.rows =  [list(item) for item in zip(*self.rows)]
-        
-        return mat
 
     def getRank(self):
         return (self.m, self.n)
@@ -228,52 +212,3 @@ class Matrix(object):
         rows = listoflists[:]
         return cls._makeMatrix(rows)
         
-
-class MatrixTests(unittest.TestCase):
-
-    def testAdd(self):
-        m1 = Matrix.fromList([[1, 2, 3], [4, 5, 6]])
-        m2 = Matrix.fromList([[7, 8, 9], [10, 11, 12]])        
-        m3 = m1 + m2
-        self.assertTrue(m3 == Matrix.fromList([[8, 10, 12], [14,16,18]]))
-
-    def testSub(self):
-        m1 = Matrix.fromList([[1, 2, 3], [4, 5, 6]])
-        m2 = Matrix.fromList([[7, 8, 9], [10, 11, 12]])        
-        m3 = m2 - m1
-        self.assertTrue(m3 == Matrix.fromList([[6, 6, 6], [6, 6, 6]]))
-
-    def testMul(self):
-        m1 = Matrix.fromList([[1, 2, 3], [4, 5, 6]])
-        m2 = Matrix.fromList([[7, 8], [10, 11], [12, 13]])
-        self.assertTrue(m1 * m2 == Matrix.fromList([[63, 69], [150, 165]]))
-        self.assertTrue(m2*m1 == Matrix.fromList([[39, 54, 69], [54, 75, 96], [64, 89, 114]]))
-
-    def testTranspose(self):
-
-        m1 = Matrix.makeRandom(25, 30)
-        zerom = Matrix.makeZero(25, 30)
-        m2 = m1 + zerom
-        
-        m1.transpose()
-        m1.transpose()
-        self.assertTrue(m2 == m1)
-
-        # Also test getTranspose
-        m2 = m1.getTranspose()
-        r2 = m2.getRank()
-
-        self.assertTrue(r2==(30,25))
-        m2.transpose()
-
-        self.assertTrue(m2 == m1)
-
-    def testId(self):
-
-        m1 = Matrix.makeId(10)
-        m2 = Matrix.makeRandom(4, 10)
-        m3 = m2*m1
-        self.assertTrue(m3 == m2)
-
-if __name__ == "__main__":
-    unittest.main()
